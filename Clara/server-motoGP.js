e5719638f480ccc33387305074205a5ef7828b73
Claracantika112 <cantikaclara131@gmail.com>
2025-10-29 18:07:17 +0700

    Add files via upload

---
+++ Clara/server-motoGP.js
@@ -0,0 +1,62 @@
+const http = require('http');
+
+const motoGP = [
+  {
+    circuit: 'Losail',
+    location: 'Qatar',
+    winner: { firstName: 'Andrea', lastName: 'Dovizioso', country: 'Italy' },
+  },
+  {
+    circuit: 'Autodromo',
+    location: 'Argentine',
+    winner: { firstName: 'Cal', lastName: 'Crutchlow', country: 'UK' },
+  },
+  {
+    circuit: 'De Jerez',
+    location: 'Spain',
+    winner: { firstName: 'Valentino', lastName: 'Rossi', country: 'Italy' },
+  },
+  {
+    circuit: 'Mugello',
+    location: 'Italy',
+    winner: { firstName: 'Andrea', lastName: 'Dovizioso', country: 'Italy' },
+  },
+];
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  if (req.url === '/') {
+    // Menampilkan seluruh data
+    res.end(JSON.stringify(motoGP, null, 2));
+
+  } else if (req.url === '/country') {
+    // Mengelompokkan berdasarkan country
+    const groupedByCountry = {};
+    motoGP.forEach(race => {
+      const country = race.winner.country;
+      if (!groupedByCountry[country]) groupedByCountry[country] = [];
+      groupedByCountry[country].push(race);
+    });
+    res.end(JSON.stringify(groupedByCountry, null, 2));
+
+  } else if (req.url === '/name') {
+    // Mengelompokkan berdasarkan nama pemenang
+    const groupedByName = {};
+    motoGP.forEach(race => {
+      const name = `${race.winner.firstName} ${race.winner.lastName}`;
+      if (!groupedByName[name]) groupedByName[name] = [];
+      groupedByName[name].push(race);
+    });
+    res.end(JSON.stringify(groupedByName, null, 2));
+
+  } else {
+    // URL selain yang ditentukan
+    res.statusCode = 400;
+    res.end(JSON.stringify({ message: 'Bad Request' }));
+  }
+});
+
+server.listen(8000, () => {
+  console.log('Server running at http://localhost:8000');
+});
